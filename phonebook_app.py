7a7d76a59000b29d5469427709f1bc060320303e
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-20 16:39:13 +0100

    init

---
+++ phonebook_app.py
@@ -0,0 +1,19 @@
+from phonebook_utilities import functions
+
+
+def main():
+    menu_choice = functions.menu()
+
+    if menu_choice == "4":
+        functions.close_app()
+    elif menu_choice == "1":
+        functions.list_phonebook()
+    elif menu_choice == "2":
+        functions.add_person()
+    elif menu_choice == "3":
+        functions.find_person()
+    else:
+        print("Wrong number try again.")
+        main()
+
+main()
---
+++ phonebook_utilities/functions.py
@@ -0,0 +1,26 @@
+def menu():
+    print("="*50, "PHONEBOOK APP: MAIN MENU", "="*50)
+    print("1. List phonebook\n"
+          "2. Add person\n"
+          "3. Find person\n"
+          "4. Exit")
+
+    user_input = input()
+    return user_input
+
+
+def list_phonebook():
+    pass
+
+
+def add_person():
+    pass
+
+
+def find_person():
+    pass
+
+
+def close_app():
+    print("Have a nice day!")
+    exit()
