e64d236a0191dc3d583b480da97b6636944e5a3e
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-21 18:03:58 +0100

    update

--- Photo_To_Excel/utilities/watermarker_utils.py
+++ Photo_To_Excel/utilities/watermarker_utils.py
@@ -1,2 +1,26 @@
-def create_thumbnails(image_list: list, images: str):
-    pass
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+
+def create_thumbnails(image_list: list, watermark_text: str):
+    photo_folder = os.path.dirname(image_list[0])
+    destination_folder = os.path.join(photo_folder, "_watermarked")
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    for photo_file in image_list:
+        img = Image.open(photo_file)
+
+        # resize image
+        img.thumbnail((500, 500))
+
+        # create watermark on image
+        draw = ImageDraw.Draw(img)
+
+        font = ImageFont.truetype("arial.ttf", 90)
+
+        draw.text((0, 120), watermark_text, fill=(255, 0, 0), font=font)
+
+        # save image to destionation
+        dest_file_name = os.path.join(destination_folder, os.path.basename(photo_file))
+        img.save(dest_file_name)
