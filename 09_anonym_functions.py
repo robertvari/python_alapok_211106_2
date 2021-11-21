9b19866130cc845c078cb0f8e2ed74931c091eeb
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-21 15:55:31 +0100

    update

--- 09_anonym_functions.py
+++ 09_anonym_functions.py
@@ -7,4 +7,10 @@ result = add_numbers(4, 10)
 result = multiply_numbers(result, 10)
 result = subdivide_numbers(result, 2)
 
-print(result)
+name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna", "Kiss Elemér Aladár"]
+number_list = [5, 2, 3, 7, 1, 87, 345, 987]
+
+print(sorted(number_list))
+print(sorted(name_list))
+
+print(sorted(name_list, key=lambda name: name.split()[-1]))
