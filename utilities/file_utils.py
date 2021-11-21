d6fba203b920958a3a5d6d109efbccab0eee5266
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-21 15:39:37 +0100

    update

--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 
 
 def get_files(root_folder: str, files=[], name_filter=None):
@@ -19,7 +19,25 @@ def get_files(root_folder: str, files=[], name_filter=None):
             if name_filter.lower() in os.path.basename(i).lower()  # condition
         ]
 
+    # store found files
+    files += all_files
+
+    # get all subfolders
+    subfolders = [
+        os.path.join(root_folder, i) for i in folder_content  # loop function
+        if os.path.isdir(os.path.join(root_folder, i))  # condition
+    ]
+
+    if subfolders:
+        for folder in subfolders:
+            get_files(folder, files, name_filter)
+
+    return files
 
 
 if __name__ == '__main__':
-    get_files(r"C:\Work\_PythonSuli\pycore-211106", name_filter="bitmap")
+    start_time = time.time()
+
+    found_files = get_files(r"C:\Work\_PythonSuli\pycore-211106")
+    print(f"Found files: {len(found_files)}")
+    print(f"Process time: {time.time()-start_time}")
