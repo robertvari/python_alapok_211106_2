87887fb3dffeed7415203cff660e7cecdbb1c75d
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-21 17:17:21 +0100

    update

--- Photo_To_Excel/main.py
+++ Photo_To_Excel/main.py
@@ -3,7 +3,9 @@ from utilities.file_utils import get_folder_path
 
 def main():
     folder_path = get_folder_path()
-    print(folder_path)
+
+    # get all .jpg files from folder
+    photo_files =
 
 
 if __name__ == '__main__':
--- Photo_To_Excel/utilities/file_utils.py
+++ Photo_To_Excel/utilities/file_utils.py
@@ -10,5 +10,39 @@ def get_folder_path():
     return user_response
 
 
+def get_files(root_folder: str, files=[], name_filter=None):
+    assert os.path.exists(root_folder), f"Folder does not exist: {root_folder}"
+
+    folder_content = os.listdir(root_folder)
+
+    # list comprehension
+    all_files = [
+        os.path.join(root_folder, i) for i in folder_content  # loop function
+        if os.path.isfile(os.path.join(root_folder, i))  # condition
+    ]
+
+    # filter found files
+    if name_filter:
+        all_files = [
+            i for i in all_files # loop function
+            if name_filter.lower() in os.path.basename(i).lower()  # condition
+        ]
+
+    # store found files
+    files += all_files
+
+    # get all subfolders
+    subfolders = [
+        os.path.join(root_folder, i) for i in folder_content  # loop function
+        if os.path.isdir(os.path.join(root_folder, i))  # condition
+    ]
+
+    if subfolders:
+        for folder in subfolders:
+            get_files(folder, files, name_filter)
+
+    return files
+
+
 if __name__ == '__main__':
     get_folder_path()
