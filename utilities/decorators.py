212412a4e7a198a349d32e2ce751906f29bcdb7a
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-21 14:22:48 +0100

    init

--- utilities/decorators.py
+++ utilities/decorators.py
@@ -6,4 +6,10 @@ def my_timer(func):
     def wrapper(*args, **kwargs):
         print(f"my_time started with {func.__name__}")
 
+        start_time = time.time()
+        result = func(*args, **kwargs)
+
+        print(f"my_time finished: {time.time()-start_time}")
+        return result
+
     return wrapper
